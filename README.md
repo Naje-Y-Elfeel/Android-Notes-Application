52b04d8f747a4056c8089383c00552fa7bccb2df
Naje-Y-Elfeel <nagi.y.alfeel@gmail.com>
2020-06-02 10:31:22 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Android Notes Application
+
+> Android Notes application built using Room, Live Data and View Model architecture components.
+
+> where you can create notes with specific priorites, read, update and swipe to delete specific note or delete all notes at once.
+
+## Preview
+![Screenshot](photo1.png)
+![Screenshot](photo2.png)
+![Screenshot](photo3.png)
+![Screenshot](photo4.png)
+![Screenshot](photo5.png)
+![Screenshot](photo6.png)
---
+++ app/src/main/java/com/example/naje/mynotes/NoteDao.java
@@ -0,0 +1,30 @@
+package com.example.naje.mynotes;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+
+    @Insert
+    void insert(Note note);
+
+    @Update
+    void update(Note note);
+
+    @Delete
+    void delete(Note note);
+
+    @Query("DELETE FROM Note")
+    void deleteAllNotes();
+
+    @Query("SELECT * FROM Note ORDER BY priority DESC")
+    LiveData<List<Note>> getAllNotes();
+
+}
